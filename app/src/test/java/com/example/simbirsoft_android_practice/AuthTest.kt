cfe455594fda44f30572ce686e3bf6851d999c87
Artem Shabunin <shuba.2001@inbox.ru>
2025-02-09 18:50:24 +0300

    Test auth callback

---
+++ app/src/test/java/com/example/simbirsoft_android_practice/AuthTest.kt
@@ -0,0 +1,21 @@
+package com.example.simbirsoft_android_practice
+
+import org.junit.Test
+
+class AuthTest {
+    @Test
+    fun testAuthCallback() {
+        val authCallback = object : AuthCallback {
+            override fun authSuccess() {
+                println("Authentication successful!")
+            }
+
+            override fun authFailed() {
+                println("Authentication failed!")
+            }
+        }
+
+        authCallback.authSuccess()
+        authCallback.authFailed()
+    }
+}
