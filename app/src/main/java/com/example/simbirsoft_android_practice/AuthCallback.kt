beb35ab6538f337d380d77cfe5d9bb0415ed1796
Artem Shabunin <shuba.2001@inbox.ru>
2025-02-09 18:47:24 +0300

    Refactor AuthCallback function signatures

--- app/src/main/java/com/example/simbirsoft_android_practice/AuthCallback.kt
+++ app/src/main/java/com/example/simbirsoft_android_practice/AuthCallback.kt
@@ -1,6 +1,6 @@
 package com.example.simbirsoft_android_practice
 
 interface AuthCallback {
-    fun authSuccess(user: User)
-    fun authFailed(reason: String)
+    fun authSuccess()
+    fun authFailed()
 }
