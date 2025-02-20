8dbc0a62ee2d3729bd65d3b490c1186a161c0628
Artem Shabunin <shuba.2001@inbox.ru>
2025-02-21 00:14:32 +0300

    Add constant for magazine type

--- app/src/main/java/com/example/simbirsoft_android_practice/Magazine.kt
+++ app/src/main/java/com/example/simbirsoft_android_practice/Magazine.kt
@@ -1,10 +1,12 @@
 package com.example.simbirsoft_android_practice
 
+private const val TYPE_MAGAZINE = "Magazine"
+
 class Magazine(
     override val price: Double,
     override val wordCount: Int
 ) : Publication {
     override fun getType(): String {
-        return "Magazine"
+        return TYPE_MAGAZINE
     }
 }
