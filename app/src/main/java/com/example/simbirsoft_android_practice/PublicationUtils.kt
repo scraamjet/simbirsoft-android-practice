9138ffc2c448ba7ff8262f9f721a51cd8cea84fd
Artem Shabunin <shuba.2001@inbox.ru>
2025-02-21 04:26:01 +0300

    Add %.2f price format for buy() function

--- app/src/main/java/com/example/simbirsoft_android_practice/PublicationUtils.kt
+++ app/src/main/java/com/example/simbirsoft_android_practice/PublicationUtils.kt
@@ -1,5 +1,6 @@
 package com.example.simbirsoft_android_practice
 
 fun buy(publication: Publication) {
-    println("The purchase is complete. The purchase amount was ${publication.price}€")
+    val formattedPrice = "%.2f".format(publication.price)
+    println("The purchase is complete. The purchase amount was $formattedPrice€")
 }
