af0f22737754f25df9b45ef24be109477877f549
Artem Shabunin <shuba.2001@inbox.ru>
2025-02-27 18:52:30 +0300

    navigate to Profile screen after Splash Screen

---
+++ app/src/main/java/com/example/simbirsoft_android_practice/SplashScreen.kt
@@ -0,0 +1,25 @@
+package com.example.simbirsoft_android_practice
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed(
+            {
+                val intent = Intent(this, ProfileActivity::class.java)
+                startActivity(intent)
+                finish()
+            },
+            2000,
+        )
+    }
+}
