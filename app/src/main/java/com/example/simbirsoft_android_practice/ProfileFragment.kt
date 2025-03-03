e9ed73d6d2cce80a961e18663eb0322b831545b1
Artem Shabunin <shuba.2001@inbox.ru>
2025-03-03 23:39:00 +0300

    implement(need refactor) delete and edit photo options

--- app/src/main/java/com/example/simbirsoft_android_practice/ProfileFragment.kt
+++ app/src/main/java/com/example/simbirsoft_android_practice/ProfileFragment.kt
@@ -1,13 +1,56 @@
 package com.example.simbirsoft_android_practice
 
+import android.app.AlertDialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import android.Manifest
+
 
 class ProfileFragment : Fragment(R.layout.fragment_profile) {
+
+    private lateinit var profileImageView: ImageView
+
+    private val cameraLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == AppCompatActivity.RESULT_OK) {
+                val imageBitmap = result.data?.extras?.get("data") as? Bitmap
+                imageBitmap?.let {
+                    profileImageView.setImageBitmap(it)
+                }
+            }
+        }
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            openCamera()
+        } else {
+            Toast.makeText(requireContext(), "Разрешение на камеру отклонено", Toast.LENGTH_SHORT)
+                .show()
+        }
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        profileImageView = view.findViewById(R.id.app_bar_image)
+
+        profileImageView.setOnClickListener {
+            showEditPhotoDialog()
+        }
 
         requireActivity().onBackPressedDispatcher.addCallback(
             viewLifecycleOwner,
@@ -16,5 +59,40 @@ class ProfileFragment : Fragment(R.layout.fragment_profile) {
                     requireActivity().finishAffinity()
                 }
             })
+    }
+
+    private fun showEditPhotoDialog() {
+        val dialogView = layoutInflater.inflate(R.layout.edit_photo_dialog, null)
+        val dialog = AlertDialog.Builder(requireContext())
+            .setView(dialogView)
+            .create()
+
+        dialogView.findViewById<LinearLayout>(R.id.take_photo)?.setOnClickListener {
+            when {
+                ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.CAMERA
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    openCamera()
+                }
+
+                else -> {
+                    requestPermissionLauncher.launch(Manifest.permission.CAMERA)
+                }
+            }
+            dialog.dismiss()
+        }
+
+        dialogView.findViewById<LinearLayout>(R.id.delete_photo)?.setOnClickListener {
+            profileImageView.setImageResource(R.drawable.ic_launcher_background)
+            dialog.dismiss()
+        }
+
+        dialog.show()
+    }
+
+    private fun openCamera() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        cameraLauncher.launch(takePictureIntent)
     }
 }
