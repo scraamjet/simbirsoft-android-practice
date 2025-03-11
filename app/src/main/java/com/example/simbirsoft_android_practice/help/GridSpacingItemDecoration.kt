56e9584852b0d753d7cf72aac957737ab8210a4a
Artem Shabunin <shuba.2001@inbox.ru>
2025-03-11 14:06:34 +0300

    Refactor GridSpacingItemDecoration

--- app/src/main/java/com/example/simbirsoft_android_practice/help/GridSpacingItemDecoration.kt
+++ app/src/main/java/com/example/simbirsoft_android_practice/help/GridSpacingItemDecoration.kt
@@ -2,12 +2,12 @@ package com.example.simbirsoft_android_practice.help
 
 import android.graphics.Rect
 import android.view.View
+import androidx.annotation.DimenRes
 import androidx.recyclerview.widget.RecyclerView
 
 class GridSpacingItemDecoration(
     private val spanCount: Int,
-    private val spacing: Int,
-    private val includeEdge: Boolean,
+    @DimenRes private val spacingDp: Int,
 ) : RecyclerView.ItemDecoration() {
     override fun getItemOffsets(
         outRect: Rect,
@@ -17,16 +17,9 @@ class GridSpacingItemDecoration(
     ) {
         val position = parent.getChildAdapterPosition(view)
         val column = position % spanCount
-
-        if (includeEdge) {
-            outRect.left = spacing - column * spacing / spanCount
-            outRect.right = (column + 1) * spacing / spanCount
-            if (position < spanCount) outRect.top = spacing
-            outRect.bottom = spacing
-        } else {
-            outRect.left = column * spacing / spanCount
-            outRect.right = spacing - (column + 1) * spacing / spanCount
-            if (position >= spanCount) outRect.top = spacing
-        }
+        outRect.left = spacingDp - column * spacingDp / spanCount
+        outRect.right = (column + 1) * spacingDp / spanCount
+        if (position < spanCount) outRect.top = spacingDp
+        outRect.bottom = spacingDp
     }
 }
