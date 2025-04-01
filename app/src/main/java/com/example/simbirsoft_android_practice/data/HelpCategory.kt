89ccd977176d9242ab076f37cecf62fbaeeccff8
Artem Shabunin <shuba.2001@inbox.ru>
2025-04-01 10:33:29 +0300

    Add @Parcelize for HelpCategory and NewsItem

--- app/src/main/java/com/example/simbirsoft_android_practice/data/HelpCategory.kt
+++ app/src/main/java/com/example/simbirsoft_android_practice/data/HelpCategory.kt
@@ -1,7 +1,11 @@
 package com.example.simbirsoft_android_practice.data
 
+import kotlinx.parcelize.Parcelize
+import android.os.Parcelable
+
+@Parcelize
 data class HelpCategory(
     val id: Int,
     val title: String,
     val iconUrl: String,
-)
+) : Parcelable
