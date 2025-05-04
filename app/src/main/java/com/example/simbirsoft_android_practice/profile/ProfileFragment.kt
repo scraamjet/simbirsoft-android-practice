e5bbcca83968ccad79a6436a9ebd9b306ef278dc
Artem Shabunin <shuba.2001@inbox.ru>
2025-05-04 04:31:25 +0300

    Refactor fragment_profile layout namings

--- app/src/main/java/com/example/simbirsoft_android_practice/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/simbirsoft_android_practice/profile/ProfileFragment.kt
@@ -63,9 +63,9 @@ class ProfileFragment : Fragment(R.layout.fragment_profile) {
             Friend(1,"Алексис Санчес", "https://photobooth.cdn.sports.ru/preset/tc_person/4/02/2c8b043f747e8b03764db15fc1d2d.png"),
             Friend(2,"Деклан Райс", "https://photobooth.cdn.sports.ru/preset/tags/3/1a/c964ab3eb44d883cca720b243570a.png"),
             Friend(3,"Букайо Сака", "https://photobooth.cdn.sports.ru/preset/tc_person/a/8b/0e7d6eba2431fa68d0275d1124d82.jpeg"),
-            Friend(4,"Алексей Гладков2", "https://thumb.tildacdn.com/tild3739-3337-4530-b562-643539663265/-/format/webp/_.jpg"),
-            Friend(5,"Филлип Киркоров2", "https://s1.bloknot-voronezh.ru/thumb/850x0xcut/upload/iblock/1ae/8r55oxgxldtr24o21gorz5bjvkqf1d74/Kirkorov.png"),
-            Friend(6,"Наруто Узумаки2", "https://arthive.net/res/media/img/orig/work/58b/7525132.jpg")
+            Friend(4,"Алексей Гладков", "https://thumb.tildacdn.com/tild3739-3337-4530-b562-643539663265/-/format/webp/_.jpg"),
+            Friend(5,"Кирилл Розов", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNtxwOwoQCubf4BzQpq4erjTloyf3O2uUblg&s"),
+            Friend(6,"Райан Гослинг", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_Gn1Em872bptcqUX2Yytct8--VEYCUv3kwQ&s")
         )
         friendAdapter.submitList(list)
     }
