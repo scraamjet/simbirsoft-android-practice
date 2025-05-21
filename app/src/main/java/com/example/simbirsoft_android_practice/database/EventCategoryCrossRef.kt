098204e4b056a545de64bda49ba25a1305a9be3b
Artem Shabunin <shuba.2001@inbox.ru>
2025-05-21 06:19:31 +0300

    Add many to many relations for entities

---
+++ app/src/main/java/com/example/simbirsoft_android_practice/database/EventCategoryCrossRef.kt
@@ -0,0 +1,21 @@
+package com.example.simbirsoft_android_practice.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+
+@Entity(
+    tableName = "event_category_cross_ref",
+    primaryKeys = ["event_id", "category_id"],
+    foreignKeys = [
+        ForeignKey(entity = EventEntity::class, parentColumns = ["id"], childColumns = ["event_id"]),
+        ForeignKey(entity = CategoryEntity::class, parentColumns = ["id"], childColumns = ["category_id"])
+    ]
+)
+data class EventCategoryCrossRef(
+    @ColumnInfo(name = "event_id")
+    val eventId: Int,
+    @ColumnInfo(name = "category_id")
+    val categoryId: Int
+)
+
