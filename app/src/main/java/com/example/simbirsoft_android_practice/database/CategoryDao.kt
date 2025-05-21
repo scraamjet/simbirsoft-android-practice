9f4aaf9a41fa940d60c707f921d6c690fbc8655c
Artem Shabunin <shuba.2001@inbox.ru>
2025-05-21 06:35:24 +0300

    Add Dao

---
+++ app/src/main/java/com/example/simbirsoft_android_practice/database/CategoryDao.kt
@@ -0,0 +1,17 @@
+package com.example.simbirsoft_android_practice.database
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface CategoryDao {
+
+    @Query("SELECT * FROM categories")
+    fun getAllCategories(): Flow<List<CategoryEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAllCategories(categories: List<CategoryEntity>)
+}
