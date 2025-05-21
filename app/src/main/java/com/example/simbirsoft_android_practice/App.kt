bb2ddb476349996d2de80b8d0f1a14eb83899be2
Artem Shabunin <shuba.2001@inbox.ru>
2025-05-21 06:46:15 +0300

    Add AppDatabase

--- app/src/main/java/com/example/simbirsoft_android_practice/App.kt
+++ app/src/main/java/com/example/simbirsoft_android_practice/App.kt
@@ -1,16 +1,33 @@
 package com.example.simbirsoft_android_practice
 
 import android.app.Application
+import androidx.room.Room
 import com.example.simbirsoft_android_practice.core.CategoryRepository
 import com.example.simbirsoft_android_practice.core.EventRepository
 import com.example.simbirsoft_android_practice.core.JsonAssetExtractor
 import com.example.simbirsoft_android_practice.core.RepositoryProvider
+import com.example.simbirsoft_android_practice.database.AppDatabase
+
+private const val DATABASE_NAME = "app_database"
 
 class App : Application(), RepositoryProvider {
+    private lateinit var database: AppDatabase
+
     override val eventRepository: EventRepository by lazy {
-        EventRepository(JsonAssetExtractor(this))
+        EventRepository(JsonAssetExtractor(this), database.eventDao())
     }
+
     override val categoryRepository: CategoryRepository by lazy {
-        CategoryRepository(JsonAssetExtractor(this))
+        CategoryRepository(JsonAssetExtractor(this), database.categoryDao())
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        database = Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java,
+            DATABASE_NAME
+        ).build()
     }
 }
+
